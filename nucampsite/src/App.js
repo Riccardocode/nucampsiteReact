04dcb15c06df92ebffb127af17438d5dcf3639af
°Riccardo <52591461+Riccardocode@users.noreply.github.com>
2023-02-18 19:09:28 +0100

    hooks

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,4 +1,8 @@
-import CampsitesList from './features/campsites/CampsitesList.js';
+import {Routes ,Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import { CAMPSITES } from './app/shared/CAMPSITES';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -8,7 +12,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path = 'contact' element={<ContactPage />} />
+          <Route path = 'directory' element= {<CampsitesDirectoryPage/>} />
+        </Routes>
       <Footer />
     </div>
   );
--- nucampsite/src/features/campsites/campsitesSlice.js
+++ nucampsite/src/features/campsites/campsitesSlice.js
@@ -2,8 +2,16 @@ import {CAMPSITES} from '../../app/shared/CAMPSITES';
 
 export const selectAllCampsites = () =>{
     return CAMPSITES;
-}
+};
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
-}
+//export const selectRandomCampsite = () => {
+//    return CAMPSITES[Math.floor(Math.random() * CAMPSITES.length)];
+//};
+
+export const selectCampsiteById = (id) =>{
+    return CAMPSITES.find((campsite) => campsite.id ===id);
+};
+
+export const selectFeaturedCampsite = () =>{
+    return CAMPSITES.find((campsite) => campsite.featured);
+};
