b153f562d3eabc628684ced49c6fa23b86416cee
°Riccardo <52591461+Riccardocode@users.noreply.github.com>
2023-02-18 13:27:52 +0100

    campsitesSlice

--- nucampsite/src/features/campsites/CampsitesList.js
+++ nucampsite/src/features/campsites/CampsitesList.js
@@ -1,13 +1,14 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
 import React from 'react';
 import {Col,Row} from 'reactstrap';
 import CampsiteCard from "./CampsiteCard"
+import {selectAllCampsites} from './campsitesSlice';
 
 const CampsitesList = () => {
+    const campsites = selectAllCampsites();
     return(
         <Row className='ms-auto'>
             {
-                CAMPSITES.map(campsite =>{
+                campsites.map(campsite =>{
                     return(
                         <Col className='m-4' md='5' key={campsite.id}>
                             <CampsiteCard campsite={campsite}/>
